54d583c6390d117b46124e812eb4767d56ac98e3
jacob.wilson <jacob.wilson@homeadvisor.com>
2019-08-29 15:38:41 -0600

    [feature] added text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Get Stankie
+          Feature Branch Changes
         </a>
       </header>
     </div>
